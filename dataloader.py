bec8a8a92e282e06e1ff5d803727fea30ff37bf4
surya sanapala <svs.surya@gmail.com>
2024-11-14 19:59:21 -0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -39,17 +39,17 @@ class PneumoniaDataset(Dataset):
                     width = row['width']
                     height = row['height']
                     boxes.append([x, y, x + width, y + height])
-                    labels.append(1)   
+                    labels.append(1)
 
             # Convert to tensors
             boxes = torch.as_tensor(boxes, dtype=torch.float32)
             labels = torch.as_tensor(labels, dtype=torch.int64)
-            
+
         if boxes.numel() == 0:
             boxes = torch.zeros((0, 4), dtype=torch.float32)
         if labels.numel() == 0:
             labels = torch.zeros((0,), dtype=torch.int64)
-            
+
         target = {}
         target['boxes'] = boxes
         target['labels'] = labels
@@ -65,14 +65,13 @@ class PneumoniaDataset(Dataset):
             image = self.transforms(image)
         return image, target
 def get_init_norm_transform():
-	transforms_list = [transforms.ToTensor()]
-	return transforms.Compose(transforms_list)
+    transforms_list = [transforms.ToTensor()]
+    return transforms.Compose(transforms_list)
 
-def get_norm_transform(mean_value,std_value):
-	transforms_list = [transforms.ToTensor()]
-	transforms_list.append(transforms.Lambda(lambda x: x.repeat(3, 1, 1)))
-	transforms_list.append(transforms.Normalize(mean=[mean_value]*3, std=[std_value]*3))
-	return transforms.Compose(transforms_list)
+def get_norm_transform(mean_value,std_value,device):
+    transforms_list=[transforms.ToTensor()]
+    transforms_list.append(transforms.Normalize(mean=mean_value.tolist(), std=std_value.tolist()))
+    return transforms.Compose(transforms_list)
 
 def get_transforms(train):
     transforms_list = [transforms.ToTensor()]
@@ -83,36 +82,36 @@ def get_transforms(train):
     return transforms.Compose(transforms_list)
 
 def get_train_dataloader_no_norm(image_dir,train_ids,validation_ids,annotations):
-	train_dataset = PneumoniaDataset(
-	    image_dir=image_dir,
-	    annotations=annotations,
-	    patient_ids=train_ids,
-	    transforms=get_init_norm_transform
-	)
+    train_dataset = PneumoniaDataset(
+        image_dir=image_dir,
+        annotations=annotations,
+        patient_ids=train_ids,
+        transforms=get_init_norm_transform()
+    )
 
-	train_loader = torch.utils.data.DataLoader(
-	    train_dataset, batch_size=4, shuffle=True, collate_fn=lambda x: tuple(zip(*x))
-	)
-	return train_loader
-def get_dataloaders_with_norm(image_dir,train_ids,validation_ids,annotations,mean_value,std_value):
-	norm_transforms=get_norm_transform(mean_value,std_value)
-	train_dataset = PneumoniaDataset(
-	    image_dir=image_dir,
-	    annotations=annotations,
-	    patient_ids=train_ids,
-	    transforms=norm_transforms
-	)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=4, shuffle=True, collate_fn=lambda x: tuple(zip(*x))
+    )
+    return train_loader
+def get_dataloaders_with_norm(image_dir,train_ids,validation_ids,annotations,mean_value,std_value,device):
+    norm_transforms=get_norm_transform(mean_value,std_value,device)
+    train_dataset = PneumoniaDataset(
+        image_dir=image_dir,
+        annotations=annotations,
+        patient_ids=train_ids,
+        transforms=norm_transforms
+    )
 
-	train_loader = torch.utils.data.DataLoader(
-	    train_dataset, batch_size=4, shuffle=True, collate_fn=lambda x: tuple(zip(*x))
-	)
-	val_dataset = PneumoniaDataset(
-	    image_dir=image_dir,
-	    annotations=annotations,
-	    patient_ids=validation_ids,
-	    transforms=norm_transforms
-	)
-	val_loader = torch.utils.data.DataLoader(
-	    val_dataset, batch_size=10, shuffle=True, collate_fn=lambda x: tuple(zip(*x))
-	)
-	return train_loader,val_loader
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=4, shuffle=True, collate_fn=lambda x: tuple(zip(*x))
+    )
+    val_dataset = PneumoniaDataset(
+        image_dir=image_dir,
+        annotations=annotations,
+        patient_ids=validation_ids,
+        transforms=norm_transforms
+    )
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=10, shuffle=True, collate_fn=lambda x: tuple(zip(*x))
+    )
+    return train_loader,val_loade
