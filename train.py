08300f09a9441e6a45c8c828a2562ddacfce9a65
ColinSulli <colinmi@umich.edu>
2024-11-23 16:38:18 -0800

    new eval

--- train.py
+++ train.py
@@ -46,24 +46,105 @@ def train(model, optimizer, train_loader, device, epoch, summary_writer, train_i
             summary_writer.add_scalar('train_loss', losses.item(), epoch * len(images) + i)
         i += 1
 
-        if i == 3000:
-            break
+        #if i == 100:
+        #    break
+
+def calculate_iou(box_1, box_2):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(box_1[0], box_2[0])
+    yA = max(box_1[1], box_2[1])
+    xB = min(box_1[2], box_2[2])
+    yB = min(box_1[3], box_2[3])
+
+    # compute the area of intersection rectangle
+    interArea = abs(max((xB - xA, 0)) * max((yB - yA), 0))
+    if interArea == 0:
+        return 0
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = abs((box_1[2] - box_1[0]) * (box_1[3] - box_1[1]))
+    boxBArea = abs((box_2[2] - box_2[0]) * (box_2[3] - box_2[1]))
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value
+    return iou
 
 def evaluate(model, valid_loader, valid_gt, device, validation_ids):
     model.eval()
-
+    iou_threshold = 0.5
     coco_c=COCO(valid_gt)
     coco_evaluator = CocoEvaluator(coco_c, iou_types=['bbox'])
     results=[]
+
+    total_positive, false_positive, false_negative = 0, 0, 0
+    correct = 0
+    ious = []
+    index = 0
     for images, targets in tqdm(valid_loader, desc=f'eval', disable=False):
+        index += 1
+
+        #if index == 100:
+        #    break
+
         images = list(img.to(device) for img in images)
         with torch.no_grad():
-            outputs = model(images)
+            prediction = model(images)
 
-            print(validation_ids[targets[0]['image_id']])
-            print(outputs)
+            '''print(validation_ids[targets[0]['image_id']])
+            print(prediction)
+            print(targets)'''
 
-            outputs = [{k: v for k, v in t.items()} for t in outputs]
+            # filter out bad scores
+            filter_threshold = 0.5
+            filtered_predictions = [
+                {key: val[torch.where(p['scores'] > filter_threshold)]
+                 for key, val in p.items()}
+                for p in prediction
+            ]
+
+            iou_threshold = 0.5
+            highest_iou = 0
+            matched_idx = set()
+            for pred_box in filtered_predictions[0]['boxes']:
+                matched = False
+                for idx, target_box in enumerate(targets[0]['boxes']):
+                    iou = calculate_iou(pred_box, target_box)
+                    if iou >= iou_threshold and idx not in matched_idx:
+                        total_positive += 1
+                        matched_idx.add(idx)
+                        matched = True
+                        ious.append(iou)
+                if not matched:
+                    false_positive += 1
+
+            if targets[0]['boxes'].numel() > 0 and filtered_predictions[0]['boxes'].numel() == 0:
+                false_negative += 1
+
+            if targets[0]['boxes'].numel() == filtered_predictions[0]['boxes'].numel():
+                correct += 1
+
+            #print(highest_iou)
+
+    # recall
+
+    print(total_positive, false_positive, false_negative)
+
+    precision = total_positive / ((total_positive + false_positive) + .001)
+    recall = total_positive / ((total_positive + false_negative) + .001)
+    average_iou = sum(ious) / (len(ious) + .001)
+    accuracy = correct / 100
+
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("Average IOU: ", average_iou)
+    print("Accuracy: ", accuracy)
+
+
+    '''outputs = [{k: v for k, v in t.items()} for t in outputs]
             for i, output in enumerate(outputs):
                 image_id = targets[i]['image_id'].item()
                 # Convert boxes to COCO format (x, y, width, height)
@@ -93,7 +174,7 @@ def evaluate(model, valid_loader, valid_gt, device, validation_ids):
         for iou_type in coco_evaluator.coco_eval:
             coco_evaluator.coco_eval[iou_type].evaluate()
             coco_evaluator.coco_eval[iou_type].accumulate()
-            coco_evaluator.coco_eval[iou_type].summarize()
+            coco_evaluator.coco_eval[iou_type].summarize()'''
 
     return coco_evaluator
     
