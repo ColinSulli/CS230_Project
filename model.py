5d64b3766b84fbe8abdccb53aa84b69b6f2073b3
ColinSulli <colinmi@umich.edu>
2024-11-24 21:57:12 -0500

    new model

--- model.py
+++ model.py
@@ -4,14 +4,14 @@ from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights
 
 def get_object_detection_model(num_classes):
 
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
-    for param in model.backbone.parameters():
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    '''for param in model.backbone.parameters():
         param.requires_grad = True
     for param in model.rpn.parameters():
         param.requires_grad = True
 
     for param in model.roi_heads.parameters():
-        param.requires_grad = True
+        param.requires_grad = True'''
     in_features = model.roi_heads.box_predictor.cls_score.in_features
 
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
