234c5837773b6cad4ebfc629fdb9d337c01e13a7
surya sanapala <svs.surya@gmail.com>
2024-11-25 20:46:47 -0800

    Update train_evaluate_starter.py

--- train_evaluate_starter.py
+++ train_evaluate_starter.py
@@ -7,7 +7,8 @@ from dataloader import get_init_norm_transform
 from dataloader import get_norm_transform
 from dataloader import get_train_dataloader_no_norm
 from train import train, evaluate
-from model import get_object_detection_model
+from model import get_object_detection_model_restnet101
+from model import get_object_detection_model_giou
 
 import torch
 import torchvision
@@ -34,22 +35,39 @@ def get_mean_std_dataset(image_dir,train_ids,validation_ids,annotations):
     mean = channel_sum / num_batches
     std = (channel_squared_sum / num_batches - mean ** 2) ** 0.5
     return mean,std
+def validate_labels_boxes(image_dir,train_ids,validation_ids,annotations):
+    t_loader=get_train_dataloader_no_norm(image_dir,train_ids,validation_ids,annotations)
+    for images, targets in t_loader:
+        for target in targets:
+            assert 'boxes' in target and 'labels' in target, f"Missing keys in target: {target.keys()}"
+            assert target['boxes'].shape[1] == 4 or target['boxes'].numel() == 0, f"Invalid boxes: {target['boxes']}"
+            assert target['labels'].dtype == torch.int64, f"Invalid labels dtype: {target['labels']}"
+            #assert target['boxes'].numel() > 0, "Empty boxes in target"
+            #assert target['labels'].numel() > 0, "Empty labels in target"
+            #print(f"Image ID: {target['image_id'].item()}, Boxes: {target['boxes'].shape}, Labels: {target['labels'].shape}")
+
 def data_init(annotations_file):
     # Setting up data
-    train_sample_size=10000
+    positive_sample_size=2000
     labels = pd.read_csv(annotations_file)
     
-    print('Total positive sample size',len(labels['Target'] == 1))
-    print('Total negative sample size',len(labels['Target'] == 0))
-    positive_patient_ids = labels[labels['Target'] == 1]['patientId'].unique()[:int(train_sample_size/2)]
-    negative_patient_ids = labels[labels['Target'] == 0]['patientId'].unique()[:int(train_sample_size/2)]
+    total_positive = len(labels[labels['Target'] == 1])
+    total_negative = len(labels[labels['Target'] == 0])
+    print(f'Total positive samples: {total_positive}')
+    print(f'Total negative samples: {total_negative}')
     np.random.seed(42)
-    np.random.shuffle(positive_patient_ids)
-    np.random.shuffle(negative_patient_ids)
+    positive_patient_ids = labels[labels['Target'] == 1]['patientId'].unique()
+    selected_positive_ids =np.random.choice(positive_patient_ids, positive_sample_size, replace=False)
+    total_positive_samples = labels[labels['patientId'].isin(selected_positive_ids)].shape[0]
+    
 
+    negative_patient_ids=np.random.choice(labels[labels['Target'] == 0]['patientId'].unique(),total_positive_samples,replace=False).tolist()
+
+    print('total_positive_patient_ids for training',total_positive_samples)
+    print('total negative_patient_ids for training',len(negative_patient_ids))
 
     positive_train_ids, positive_val_ids = train_test_split(
-        positive_patient_ids,
+        selected_positive_ids,
         train_size=0.8,
         random_state=42,
         shuffle=True
@@ -62,7 +80,6 @@ def data_init(annotations_file):
         random_state=42,
         shuffle=True
     )
-
     # Combine for training and validation
     patient_ids_train = list(positive_train_ids) + list(negative_train_ids)
     patient_ids_validation = list(positive_val_ids) + list(negative_val_ids)
@@ -70,10 +87,12 @@ def data_init(annotations_file):
     print('size of patient_ids_validation',len(patient_ids_validation))
     return patient_ids_train,patient_ids_validation,labels
 def train_and_evaluate(train_data_loader,val_loader,device,epochs,valid_gt) :
-    model=get_object_detection_model(2)
+    model=get_object_detection_model_giou(2)
     model.to(device)
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.00084232, momentum=0.9003619, weight_decay=0.00001106)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    print('model initialized')
+    #optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9323368245702841, weight_decay=0.0001298489873419346)
+    optimizer=torch.optim.Adam(model.parameters(), lr=0.00001)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
     best_val_map=0.0
     torch.autograd.set_detect_anomaly(True)
     val_maps = []
@@ -81,8 +100,11 @@ def train_and_evaluate(train_data_loader,val_loader,device,epochs,valid_gt) :
         print('runnng epoch:',epoch)
         train(model, optimizer, train_data_loader, device, epoch)
         lr_scheduler.step()
-        coco_evaluator = evaluate(model, val_loader,valid_gt, device=device)
-
+        try:
+            coco_evaluator = evaluate(model, val_loader,valid_gt, device=device)
+        except Exception as e:
+            print(f"An exception occurred: {e}")
+            raise e
         stats = coco_evaluator.coco_eval['bbox'].stats
         val_map = stats[0]
         val_maps.append(val_map)
@@ -95,12 +117,17 @@ def train_and_evaluate(train_data_loader,val_loader,device,epochs,valid_gt) :
     plt.legend()
     plt.show()
 if __name__ == "__main__":
-    annotations_file='~/Documents/CS230/Project/rsna-pneumonia-detection-challenge/stage_2_train_labels.csv'
-    image_dir='/Users/suryasanapala/Documents/CS230/Project/rsna-pneumonia-detection-challenge/stage_2_train_images'
-    num_epochs=10
-    device=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    annotations_file='stage_2_train_labels.csv'
+    image_dir='/home/ec2-user/cs230/stage_2_train_images'
+
+    num_epochs=30
+    device=torch.device('cpu') 
+    if torch.cuda.is_available():
+        device=torch.device('cuda')
+    print('Running on device',device)
     train_ids,validation_ids,annotations=data_init(annotations_file)
+    #validate_labels_boxes(image_dir,train_ids,validation_ids,annotations)
     mean,std=get_mean_std_dataset(image_dir,train_ids,validation_ids,annotations)
-    train_loader,valid_loader= get_dataloaders_with_norm(image_dir,train_ids,validation_ids,annotations,mean,std,device)
+    train_loader,valid_loader= get_dataloaders_with_norm(image_dir,train_ids,validation_ids,annotations,mean,std,device,True)
     coco_format_validation_ds=convert_evalset_coco(validation_ids,annotations,'./')
     train_and_evaluate(train_loader,valid_loader,device,num_epochs,coco_format_validation_ds)
